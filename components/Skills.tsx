309e2cdfc6be27b08c7d671536e39d574f3b8535
Vishi Tyagi <vishi.tyagi@techaheadcorp.com>
2026-01-26 17:17:19 +0530

    style: enhanced skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,15 +1,48 @@
 import skills from "@/data/skills";
 import styles from "@/styles/skills.module.css";
+import Image from "next/image";
 
 export default function Skills() {
   return (
     <section className={styles.skills}>
-      <h2>Skills</h2>
+      <div className="flex">
+        <div className="w-1/3">
+          <h2>Technical Arsenal</h2>
+        </div>
+        <div className="w-1/3"></div>
+        <div className="w-2/3 text-right" style={{ alignContent: "flex-end" }}>
+          <p>A curated set of technologies I use to build scalable, high-performance applications.</p>
+        </div>
+      </div>
+
+      <hr />
 
       <div className={styles.grid}>
-        {Object.entries(skills).map(([category, list]) => (
+        {Object.entries(skills).map(([category, { image, list }]) => (
           <div key={category} className={styles.card}>
+
+            <div className={styles.imageCard}>
+              <Image
+                src={image}
+                alt={category}
+                className={styles.skillImage}
+                width={32}
+                height={32}
+              />
+            </div>
             <h3>{category}</h3>
+
+            <div className={styles.progressBar}>
+              <div
+                style={{
+                  width: '80%',
+                  backgroundColor: '#556a8d',
+                  borderRadius: '1rem',
+                  border: '1rem'
+                }}
+              />
+            </div>
+
             <ul>
               {list.map((skill) => (
                 <li key={skill}>{skill}</li>
--- data/skills.ts
+++ data/skills.ts
@@ -1,8 +1,8 @@
 const skills = {
-  Backend: ["Node.js", "NestJS", "Express.js"],
-  Frontend: ["React.js", "Angular"],
-  Databases: ["MySQL", "PostgreSQL", "MongoDB"],
-  Other: ["REST APIs", "JWT Authentication", "Git"]
-};
+  Backend: {image: "https://static.wixstatic.com/media/b2ed21_051de5eb67e94f48b49a5eddf2ef1455~mv2.png/v1/fill/w_48,h_48,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/b2ed21_051de5eb67e94f48b49a5eddf2ef1455~mv2.png", list: ["Node.js", "NestJS", "Express.js"]},
+  Frontend: {image: "https://static.wixstatic.com/media/b2ed21_8068d8e5881740ee951a2712d201ff12~mv2.png/v1/fill/w_48,h_48,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/b2ed21_8068d8e5881740ee951a2712d201ff12~mv2.png", list: ["React.js", "Angular"]},
+  Databases: {image: "https://static.wixstatic.com/media/b2ed21_bdf8142537ee4176b802d5b21a0221f2~mv2.png/v1/fill/w_48,h_48,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/b2ed21_bdf8142537ee4176b802d5b21a0221f2~mv2.png", list: ["MySQL", "PostgreSQL", "MongoDB"]},
+  Other: {image: "https://static.wixstatic.com/media/b2ed21_e187366bdb4c4b8d9c331fc8e6091013~mv2.png/v1/fill/w_48,h_48,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/b2ed21_e187366bdb4c4b8d9c331fc8e6091013~mv2.png", list: ["Git", "AWS", "Docker", "Third Party APIs"]}
+};   
 
 export default skills;
